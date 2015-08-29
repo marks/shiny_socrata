10aca68224da9b13af856ccbed48ea2528ab9f2c
Mark Silverberg <mark.silverberg@socrata.com>
2015-08-29 19:48:38 -0400

    initial prototype

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(RSocrata) # Stata and SPSS
+library(ggplot2)
 
 options(shiny.maxRequestSize=30*1024^2) # via http://stackoverflow.com/a/18037912/252671
 
@@ -15,8 +15,36 @@ shinyServer(function(input, output) {
     }
   })
 
-  output$table <- renderTable({
-    datasetInput()
+  output$axisSelectors <- renderUI({
+    if(is.null(input$url)){}
+    else {
+      list(
+        selectizeInput("xAxisSelector", "X Axis Variable:",
+            colnames(datasetInput())),
+        selectizeInput("yAxisSelector", "Y Axis Variable:",
+            colnames(datasetInput()))
+      )      
+    }
   })
 
+  output$table <- renderTable({
+    head(datasetInput(),n=20)
+  })
+
+  output$scatterplot <- renderPlot({
+    x <- as.symbol(input$xAxisSelector)
+    y <- as.symbol(input$yAxisSelector)
+    qplot(eval(x),eval(y),data=datasetInput(), xlab=x,ylab=y)
+  })
+
+  output$boxplot <- renderPlot({
+    x <- as.symbol(input$xAxisSelector)
+    y <- as.symbol(input$yAxisSelector)
+    boxplot(eval(y)~eval(x),data=datasetInput()) #,xlab=x,ylab=y)
+  })
+
+
+
 })
+
+
--- ui.R
+++ ui.R
@@ -1,10 +1,29 @@
 shinyUI(fluidPage(
   tags$head(includeScript("google-analytics.js")),
-  headerPanel("Read from CSV URL"),
+  headerPanel(
+  	h1("Analyze CSV Data")
+  ),
   sidebarPanel(
-    textInput("url", "URL to CSV", "http://soda.demo.socrata.com/resource/4334-bgaj.csv")
+    textInput("url", "URL to CSV", "http://data.cms.gov/resource/kcsi-wmjs.csv"),
+    uiOutput('axisSelectors')
   ),
   mainPanel(
-    tableOutput('table')
+  	h2("Plots"),
+		fluidRow(
+		    column(width = 6,
+		    	h3("Scatterplot"),
+		      plotOutput("scatterplot", height = 300,)
+		    ),
+		    column(width = 6,
+		    	h3("Boxplot"),
+		      plotOutput("boxplot", height = 300,)
+		    )
+		 ),
+	 h2("Data Sample"),
+	 helpText("Only the first twenty rows are shown below"),
+   tableOutput('table')
   )
 ))
+
+
+
