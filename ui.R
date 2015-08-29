eaa8d317fa45a3ee785e931152e335396e38a09c
Mark Silverberg <mark.silverberg@socrata.com>
2015-08-29 19:52:39 -0400

    initial prototype

--- ui.R
+++ ui.R
@@ -1,8 +1,8 @@
 shinyUI(fluidPage(
   tags$head(includeScript("google-analytics.js")),
   headerPanel(
-  	h1("Analyze CSV Data")
-  	helpText("built by Mark Silverberg @ Socrata | @skram | mark.silverberg@socrata.com")
+  	h1("Analyze CSV Data"),
+  	helpText("built by Mark Silverberg @ Socrata | @skram | mark.silverberg@socrata.com"),
   ),
   sidebarPanel(
     textInput("url", "URL to CSV", "http://data.cms.gov/resource/kcsi-wmjs.csv"),
