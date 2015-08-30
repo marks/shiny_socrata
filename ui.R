2b671a5993b80cdc8e1ee5425dcc78c46db8d1f5
Mark Silverberg <mark.silverberg@socrata.com>
2015-08-30 13:42:08 -0400

    switch URLs to use HTTP as shinyapps.io does not support HTTPS

--- ui.R
+++ ui.R
@@ -11,8 +11,8 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
 		helpText("Examples:"),
 		helpText(HTML("<ul>
 			<li>data.cms.gov 2014 ACO performance data: ACO-1 by Track: <a href='?url=http://data.cms.gov/resource/kcsi-wmjs.csv&xAxis=track&yAxis=aco_1'>?url=http://data.cms.gov/resource/kcsi-wmjs.csv&xAxis=track&yAxis=aco_1</a></li>
-			<li>data.medicare.gov State Hospital Acquired Infection (HAI) scores: <a href='?url=https://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score'>?url=https://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score</a></li>
-			<li>data.cityofchicago.org Affordable Housing capacity by type: <a href='?url=https://data.cityofchicago.org/resource/s6ha-ppgi.csv&xAxis=Property.Type&yAxis=Units'>?url=https://data.cityofchicago.org/resource/s6ha-ppgi.csv&xAxis=Property.Type&yAxis=Units</a></li>
+			<li>data.medicare.gov State Hospital Acquired Infection (HAI) scores: <a href='?url=http://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score'>?url=http://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score</a></li>
+			<li>data.cityofchicago.org Affordable Housing capacity by type: <a href='?url=http://data.cityofchicago.org/resource/s6ha-ppgi.csv&xAxis=Property.Type&yAxis=Units'>?url=http://data.cityofchicago.org/resource/s6ha-ppgi.csv&xAxis=Property.Type&yAxis=Units</a></li>
 			</ul>"))
 	),
 	mainPanel(
