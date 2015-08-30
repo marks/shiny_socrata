50b47e02de7aebfdf8d860c2c9550a8342f7ff79
Mark Silverberg <mark.silverberg@socrata.com>
2015-08-30 13:36:43 -0400

    change boxplot to use qplot so it too can be zoomed into

--- server.R
+++ server.R
@@ -6,7 +6,6 @@ options(shiny.maxRequestSize=30*1024^2) # via http://stackoverflow.com/a/1803791
 shinyServer(function(input, output, session) {
 
   datasetInput <- reactive({
-
     # parse URL query and adjust text fields accordingly
     urlQuery <- parseQueryString(session$clientData$url_search)
     if(!is.null(urlQuery$url)){ 
@@ -18,7 +17,6 @@ shinyServer(function(input, output, session) {
     if(!is.null(urlQuery$yAxis)){
       updateSelectizeInput(session, "yAxisSelector", selected = urlQuery$yAxis)
     }
-
     # read text box's URL, read, and store in data frame which is returned
     inURL <- input$url
     if(is.null(inURL)){
@@ -29,6 +27,7 @@ shinyServer(function(input, output, session) {
     }
   })
 
+  ## Axis Selectors
   output$axisSelectors <- renderUI({
     if(is.null(input$url)){}
     else {
@@ -41,21 +40,54 @@ shinyServer(function(input, output, session) {
     }
   })
 
+  ## Datatable
   output$table <- renderTable({
     head(datasetInput(),n=20)
   })
 
+
+  ## Interactive Scatterplot (adopted from http://shiny.rstudio.com/gallery/plot-interaction-zoom.html)
+  scatterplot_ranges <- reactiveValues(scatterplot_x = NULL, scatterplot_y = NULL)
   output$scatterplot <- renderPlot({
     x <- as.symbol(input$xAxisSelector)
     y <- as.symbol(input$yAxisSelector)
-    qplot(eval(x),eval(y),data=datasetInput(), xlab=x,ylab=y)
+    qplot(eval(x),eval(y),data=datasetInput(), xlab=x,ylab=y) + 
+      geom_point() +
+      coord_cartesian(xlim = scatterplot_ranges$scatterplot_x, ylim = scatterplot_ranges$scatterplot_y)
+  })
+  observeEvent(input$scatterplot_dblclick, {
+    brush <- input$scatterplot_brush
+    if (!is.null(brush)) {
+      scatterplot_ranges$scatterplot_x <- c(brush$xmin, brush$xmax)
+      scatterplot_ranges$scatterplot_y <- c(brush$ymin, brush$ymax)
+
+    } else {
+      scatterplot_ranges$scatterplot_x <- NULL
+      scatterplot_ranges$scatterplot_y <- NULL
+    }
   })
 
+  ## Boxplot
+  boxplot_ranges <- reactiveValues(boxplot_x = NULL, boxplot_y = NULL)
   output$boxplot <- renderPlot({
     x <- as.symbol(input$xAxisSelector)
     y <- as.symbol(input$yAxisSelector)
-    boxplot(eval(y)~eval(x),data=datasetInput(), xlab=input$xAxisSelector,ylab=input$yAxisSelector)
+    qplot(eval(x),eval(y),data=datasetInput(), xlab=x,ylab=y) + 
+      geom_boxplot() +
+      coord_cartesian(xlim = boxplot_ranges$boxplot_x, ylim = boxplot_ranges$boxplot_y)
   })
+  observeEvent(input$boxplot_dblclick, {
+    brush <- input$boxplot_brush
+    if (!is.null(brush)) {
+      boxplot_ranges$boxplot_x <- c(brush$xmin, brush$xmax)
+      boxplot_ranges$boxplot_y <- c(brush$ymin, brush$ymax)
+
+    } else {
+      boxplot_ranges$boxplot_x <- NULL
+      boxplot_ranges$boxplot_y <- NULL
+    }
+  })
+
 
 })
 
--- ui.R
+++ ui.R
@@ -9,20 +9,38 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
 		hr(),
 		helpText("Psst: you can pass the following url parameters: url, xAxis, yAxis."),
 		helpText("Examples:"),
-		helpText(HTML("<ul><li>data.cms.gov 2014 ACO performance data: ACO-1 by Track: <a href='/?url=http://data.cms.gov/resource/kcsi-wmjs.csv&xAxis=track&yAxis=aco_1'>/?url=http://data.cms.gov/resource/kcsi-wmjs.csv&xAxis=track&yAxis=aco_1</a></li><li>data.medicare.gov State Hospital Acquired Infection (HAI) scores: <a href='/?url=https://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score'>/?url=https://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score</a></li>"))
+		helpText(HTML("<ul>
+			<li>data.cms.gov 2014 ACO performance data: ACO-1 by Track: <a href='/?url=http://data.cms.gov/resource/kcsi-wmjs.csv&xAxis=track&yAxis=aco_1'>/?url=http://data.cms.gov/resource/kcsi-wmjs.csv&xAxis=track&yAxis=aco_1</a></li>
+			<li>data.medicare.gov State Hospital Acquired Infection (HAI) scores: <a href='/?url=https://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score'>/?url=https://data.medicare.gov/resource/k2ze-bqvw.csv&xAxis=Measure.Name&yAxis=Score</a></li>
+			<li>data.cityofchicago.org Affordable Housing capacity by type: <a href='/?url=https://data.cityofchicago.org/resource/s6ha-ppgi.csv&xAxis=Property.Type&yAxis=Units'>/?url=https://data.cityofchicago.org/resource/s6ha-ppgi.csv&xAxis=Property.Type&yAxis=Units</a></li>
+			</ul>"))
 	),
 	mainPanel(
 		h2("Plots"),
 		fluidRow(
 			column(width = 6,
 				h3("Scatterplot"),
-				plotOutput("scatterplot", height = 300,)
+	      plotOutput("scatterplot", height = 350,
+	        dblclick = "scatterplot_dblclick",
+	        brush = brushOpts(
+	          id = "scatterplot_brush",
+	          resetOnNew = TRUE
+	        )
+	      )
 			),
 			column(width = 6,
 				h3("Boxplot"),
-				plotOutput("boxplot", height = 300,)
+	      plotOutput("boxplot", height = 350,
+	        dblclick = "boxplot_dblclick",
+	        brush = brushOpts(
+	          id = "boxplot_brush",
+	          resetOnNew = TRUE
+	        )
+	      )
 			)
 		),
+		helpText("You can click, drag, and double click to zoom into the scatterplot. Double click to zoom back out to original extent."),
+		hr(),
 		h2("Data Sample"),
 		helpText("Only the first twenty rows are shown below"),
 		dataTableOutput('table')
